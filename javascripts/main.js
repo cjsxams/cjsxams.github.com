5663668d6c705329058edfb5ae4e2190239fe396
joexams <joe_xams@hotmail.com>
2015-03-11 10:41:25 +0800

    1
    1

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,15 +1,14 @@
 jQuery.noConflict();
 //2种方式调用
 //html5直接调用
-//jQuery('#qrcode').qrcode(window.location.href);
+jQuery('#qrcode').qrcode(window.location.href);
 //不支持html5,可配置图片的尺寸
-jQuery("#qrcode").qrcode({
-    render: "canvas", //table方式
-    width: 150, //宽度
-    height:150, //高度
-    text: 'http://www.baidu.com/' //任意内容
-});
-alert(234);
+//jQuery("#qrcode").qrcode({
+//    render: "canvas", //table方式
+//    width: 150, //宽度
+//    height:150, //高度
+//    text: 'http://www.baidu.com/' //任意内容
+//});
 function showHideCode(){
 	$("#showdiv").toggle();
 }
